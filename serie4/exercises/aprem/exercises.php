181077da38b3e5d716a40ce35a1948a8c391a323
yunities-front <reubenCFormation@gmail.com>
2023-05-28 10:27:18 +0200

    modify wording

--- serie4/exercises/aprem/exercises.php
+++ serie4/exercises/aprem/exercises.php
@@ -2,7 +2,7 @@
 //1) creer un fichier header.php qui contiendra deux liens.Un lien pointera vers sports.php et un deuxieme lien pointera vers students.php
 //2)Creer un fichier sports.php et en utlisant les données de data.php contenu dans le dossier utils, faites en sorte d'afficher sous forme de tableau html ou de list-group le nom de chaque sport et listez aussi les noms de chaque jouer qui joue le sport (selon les données dans data.php) Pour lister tous les jouers qui joue le sport pensez au boucle imbriqués!. Vous devez obligatoirement utiliser un require!
 //3) creer un fichier students.php et en utilisant les données de data.php contenu dans le dossier utils, faites en sorte d'affoicher sous forme de tableu ou de list-group le nom, l'age et la moyenne de chacun de nos etudiants. Vous devez obligatoirement utilisez un require!
-//4) faites en sorte de pouvoir avoir un header dans le quelle nous pouvons changer de fichier (soit nous allons consulter sports.php soit nous allons consulter students.php)
+//4) faites en sorte de pouvoir avoir un header dans les deux fichiers (sports.php et students.pjp) le quelle nous pouvons changer de fichier (soit nous allons consulter sports.php soit nous allons consulter students.php)
 
 // BONUS!!!
 // pour le fichier sports.php faites en sorte d'afficher les information du sport en jaune si le sport est un sport contact. Sinon, affichez les informations en vert.
